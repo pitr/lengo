471991250a134f1b79da74fafea67d5cd856ef75
pitr <pitr.vern@gmail.com>
2012-12-01 15:29:23 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idealog::Application.config.secret_token = '159849b19fda1ab4209c422cb2977c0b6d0c56825e539730d2f58a31b8469d1f400e8c4657f134eff049309b3dade61c6e3d1f6a39da0fe6866d3e6791ccf3a5'
