4d7651171b0b172accb7337d9bd6bd0440d461f5
pitr <pitr.vern@gmail.com>
2012-12-02 08:54:42 -0500

    cleanup controller a bit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,18 +1,13 @@
 class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @ideas }
-    end
   end
 
   def show
     @idea = Idea.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.json { render json: @idea }
     end
   end
@@ -20,20 +15,10 @@ class IdeasController < ApplicationController
   def create
     @root_idea = Idea.create_from_json(params[:idea])
 
-    render json: @root_idea, status: :created, location: @root_idea
+    render :template => "ideas/completed"
   end
 
-  def update
-    @idea = Idea.find(params[:id])
-
-    respond_to do |format|
-      if @idea.update_attributes(params[:idea])
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
-    end
+  def completed
+    @root_idea = Idea.find_by_id(3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,7 @@ Idealog::Application.routes.draw do
 
   get "speech/tts"
 
-  # authenticated :user do
-    root :to => 'ideas#index'
+  root :to => 'ideas#index'
 
-    resources :ideas
-  # end
-
-  # root :to => "home#index"
-
-  # devise_for :users
-
-  # resources :users
+  resources :ideas
 end
